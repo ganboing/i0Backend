da686108764384d5723ff40ea946d47214207ff2
foobar <foo@bar.cc>
2015-05-03 15:03:46 +0800

    modify build script

--- buildscript/confi0.eclipse.sh
+++ buildscript/confi0.eclipse.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-current_dir=$(dirname "$0")
-llvm_source_dir=$(realpath "$current_dir/llvm.i0")
-llvm_eclipse_dir=$(realpath "$current_dir/llvm.i0.eclipse")
+llvm_dir=$1
+llvm_source_dir=$(realpath "$llvm_dir/llvm.i0")
+echo "llvm source from $llvm_source_dir"
+llvm_eclipse_dir=$(realpath "$llvm_dir/llvm.i0.eclipse")
+echo "llvm eclips project dir $llvm_eclipse_dir"
 cd $llvm_eclipse_dir
 cmake -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_ECLIPSE_VERSION=4.2 -DCMAKE_CXX_COMPILER_ARG1=-std=c++11 -DCMAKE_BUILD_TYPE=Debug -DLLVM_TARGETS_TO_BUILD=i0  $llvm_source_dir
--- buildscript/confi0.sh
+++ buildscript/confi0.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-current_dir=$(dirname "$0")
-llvm_source_dir=$(realpath "$current_dir/llvm.i0")
-llvm_build_dir=$(realpath "$current_dir/llvm.i0.build")
-llvm_install_dir=$(realpath "$current_dir/llvm.i0.install")
+llvm_dir=$1
+llvm_source_dir=$(realpath "$llvm_dir/llvm.i0")
+echo "llvm source from $llvm_source_dir"
+llvm_build_dir=$(realpath "$llvm_dir/llvm.i0.build")
+echo "build to $llvm_build_dir"
+llvm_install_dir=$(realpath "$llvm_dir/llvm.i0.install")
+echo "install to $llvm_install_dir"
 cd $llvm_build_dir
 cmake -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DCMAKE_BUILD_TYPE=Debug -DLLVM_TARGETS_TO_BUILD=i0 -DCMAKE_INSTALL_PREFIX=$llvm_install_dir $llvm_source_dir
  
