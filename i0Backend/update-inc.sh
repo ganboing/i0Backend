9268463268019ab133f903e343ae891f0f8a847c
foo@vm <foobar@cc>
2015-05-05 17:18:21 +0800

    update script

---
+++ i0Backend/update-inc.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+i0_build_dir=$(dirname $0)/../../../../objs/llvm.i0.build/lib/Target/i0
+echo "copying tablegen inc from $(realpath $i0_build_dir)"
+cp i0_build_dir/*.inc $(dirname $0)/
+
