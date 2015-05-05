05563a618cd9cac95aeb0cb1e1912639ca7577e3
foo@vm <foobar@cc>
2015-05-05 13:29:07 +0800

    modify script

--- buildscript/confi0.sh
+++ buildscript/confi0.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 llvm_dir=$1
-mkdir "$llvm_dir/llvm.i0"
 llvm_source_dir=$(realpath "$llvm_dir/llvm.i0")
 echo "llvm source from $llvm_source_dir"
-mkdir "$llvm_dir/llvm.i0.build"
-llvm_build_dir=$(realpath "$llvm_dir/llvm.i0.build")
+mkdir "$llvm_dir/objs/llvm.i0.build"
+llvm_build_dir=$(realpath "$llvm_dir/objs/llvm.i0.build")
 echo "build to $llvm_build_dir"
-mkdir "$llvm_dir/llvm.i0.install"
-llvm_install_dir=$(realpath "$llvm_dir/llvm.i0.install")
+mkdir "$llvm_dir/objs/llvm.i0.install"
+llvm_install_dir=$(realpath "$llvm_dir/objs/llvm.i0.install")
 echo "install to $llvm_install_dir"
 cd $llvm_build_dir
 cmake -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DCMAKE_BUILD_TYPE=Debug -DLLVM_TARGETS_TO_BUILD=i0 -DCMAKE_INSTALL_PREFIX=$llvm_install_dir $llvm_source_dir
