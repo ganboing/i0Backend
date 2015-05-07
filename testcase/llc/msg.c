896048dd27e01e9a252a15fdf325fe0c74d1bd1e
ganboing <ganboing@gmail.com>
2015-05-07 09:01:22 +0800

    add testcase

---
+++ testcase/llc/msg.c
@@ -0,0 +1,19 @@
+//#include "libi0/stddef.h"
+
+#define STDIN   (0x100000200ULL)
+#define STDOUT  (0x100000208ULL)
+
+void test(void){
+char msg[] = "hello";
+for(unsigned i=0;i<sizeof(msg)/sizeof(msg[0]);++i){
+*(volatile long*)STDOUT = msg[i];
+}
+}
+
+void test2(void){
+char msg[] = "world";
+for(unsigned i=0;i<sizeof(msg)/sizeof(msg[0]);++i){
+*(volatile long*)STDOUT = msg[i];
+}
+}
+
